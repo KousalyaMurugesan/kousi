8b59902f3f1b8daf49dc55cef553150ec91ae76e
kousalya <kousalyamurugesan6@gmail.com>
2016-08-12 21:36:23 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+
+}
