da0e1536b8fab29d666826947fa6a4faa018ee12
kousalya <kousalyamurugesan6@gmail.com>
2016-08-12 21:40:09 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a<0)
+		{
+			System.out.println("negative");
+		}
+		else
+		{
+			System.out.println("positive");
+		}
+	}
+
+}
