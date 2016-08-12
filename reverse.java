44f07f541fe93a40105314f19e60daf5b82efd7e
kousalya <kousalyamurugesan6@gmail.com>
2016-08-12 21:45:58 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		StringBuffer st=new StringBuffer(a);
+		st.reverse();
+		System.out.println(st.toString());
+	}
+
+}
