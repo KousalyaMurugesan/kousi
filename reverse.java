774311c69a0991e56ccfdd7cfc9fec22f98cd4ed
kousalya <kousalyamurugesan6@gmail.com>
2016-08-13 10:47:21 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,14 +1,23 @@
 import java.util.*;
-import java.io.*;
-public class hello {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s=new Scanner(System.in);
-		String a=s.next();
-		StringBuffer st=new StringBuffer(a);
-		st.reverse();
-		System.out.println(st.toString());
+import java.lang.*;
+public class reverse
+{
+public static void main(String[] args)
+  {
+	String s1="VowelsRemoved";
+	System.out.println(s1);
+	StringBuffer str=new StringBuffer(s1);
+	str=str.reverse();
+	System.out.println(str.toString());
+	String s=str.toString();
+	StringBuffer str2=new StringBuffer(s);
+	for(int i=0;i<str2.length();i++)
+	{
+		if(str2.charAt(i)=='a'||str2.charAt(i)=='e'||str2.charAt(i)=='i'||str2.charAt(i)=='o'||str2.charAt(i)=='u')
+		{
+			str2.deleteCharAt(i);
+		}
 	}
-
+System.out.println(str2.toString());
+  }
 }
