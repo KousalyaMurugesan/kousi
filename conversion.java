7e9d93e6c43fd9171bf359ce69039f5c655b3924
kousalya <kousalyamurugesan6@gmail.com>
2016-08-28 11:49:18 +0530

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class conversion
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+int a=Integer.parseInt(s1);
+System.out.println("Integer value:"+a);
+}
+}
